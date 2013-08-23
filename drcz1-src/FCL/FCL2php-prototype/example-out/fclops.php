75f136108093bf9335a752d952dc960687fce0b1
Beata Kozidrak <drcz@konfident.(none)>
2013-08-23 12:13:33 +0200

    fclops.php's small fix

--- drcz1-src/FCL/FCL2php-prototype/example-out/fclops.php
+++ drcz1-src/FCL/FCL2php-prototype/example-out/fclops.php
@@ -1,11 +1,8 @@
 <?php
-
-function SE_car($e) { return $e[0]; }
+function SE_car($e) { if(count($e)>0) return $e[0]; else return null; }
 function SE_cdr($e) { array_shift($e); return $e; }
 function SE_cons($h,$t) {
     array_unshift($t,$h);
     return $t;
 }
-
-
 ?>
